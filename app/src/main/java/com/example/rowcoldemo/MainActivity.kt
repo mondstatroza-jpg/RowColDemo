00b1abb9d28fc68214949da567b836f4d4b9b8f9
mondstatroza-jpg <mondstatroza@gmail.com>
2026-09-19 12:17:16 +0700

    Initial project setup

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RowColDemoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "AAndroid",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
